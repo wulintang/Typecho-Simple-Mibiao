29482e99a38b59d9431e4c5343550fda32415cd2
懋和道人 <15062733214@qq.com>
2024-06-20 23:54:59 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,22 +1,24 @@
 <?php
-// site root path
+
+/** 开启https */
+define('__TYPECHO_SECURE__',true);
+/** 定义根目录 */
 define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
-
-// plugin directory (relative path)
+/** 定义插件目录(相对路径) */
 define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
-
-// theme directory (relative path)
+/** 定义模板目录(相对路径) */
 define('__TYPECHO_THEME_DIR__', '/usr/themes');
-
-// admin directory (relative path)
+/** 后台路径(相对路径) */
 define('__TYPECHO_ADMIN_DIR__', '/admin/');
-
-// register autoload
-require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
-
-// init
-\Typecho\Common::init();
-
+/** 设置包含路径 */
+@set_include_path(get_include_path() . PATH_SEPARATOR .
+__TYPECHO_ROOT_DIR__ . '/var' . PATH_SEPARATOR .
+__TYPECHO_ROOT_DIR__ . __TYPECHO_PLUGIN_DIR__);
+/** 载入API支持 */
+require_once 'Typecho/Common.php';
+/** 程序初始化 */
+Typecho_Common::init();
+/** 定义数据库参数 */
 // config db
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
